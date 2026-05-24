2b198a4ed528024e876f575a0fd73fce105aa109
Chaeyeong_10 <147240485+Chaeyeonggg@users.noreply.github.com>
2026-05-24 17:53:18 +0900

    [level 0] Title: 배열 만들기 4, Time: 52.45 ms, Memory: 13.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
@@ -0,0 +1,14 @@
+def solution(arr):
+    stk = []
+    i = 0
+    while i < len(arr):
+        if len(stk) == 0:
+            stk.append(arr[i])
+            i += 1
+        elif len(stk) != 0 and stk[len(stk)-1] >= arr[i]:
+            del stk[len(stk)-1]
+        else:
+            stk.append(arr[i])
+            i += 1
+    
+    return stk
